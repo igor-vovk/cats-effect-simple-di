1985ae8ca14d4f3d41356fb6f26d5b0b672eefe0
Ihor Vovk <adWolf15@gmail.com>
2025-02-15 23:19:52 +0100

    Use Dispatcher and tagless instead of direct IORuntime operations (#42)

--- src/main/scala/io/github/cats_effect_simple_di/Allocator.scala
+++ src/main/scala/io/github/cats_effect_simple_di/Allocator.scala
@@ -1,52 +1,60 @@
 package io.github.cats_effect_simple_di
 
+import cats.effect.implicits.*
+import cats.effect.std.Dispatcher
 import cats.effect.unsafe.IORuntime
-import cats.effect.{IO, Ref, Resource}
+import cats.effect.*
+import cats.implicits.*
 
 import scala.reflect.ClassTag
 
 object Allocator {
 
-  def create(
-    runtime: IORuntime,
-    listener: AllocationLifecycleListener = NoOpListener,
-  ): Resource[IO, Allocator] = {
-    val acquire = IO(unsafeCreate(runtime, listener))
-    val release = (a: Allocator) => a.shutdownAll
-    Resource.make(acquire)(release)
-  }
+  def create[F[_]: Async](): Resource[F, Allocator[F]] =
+    for
+      dispatcher  <- Dispatcher.parallel[F]
+      shutdownRef <- Ref.of(Async[F].unit).toResource
+      allocator <- {
+        val acquire = Async[F].delay(Allocator(dispatcher, shutdownRef, NoOpListener[F]))
+        val release = (a: Allocator[F]) => a.shutdownAll
+
+        Resource.make(acquire)(release)
+      }
+    yield allocator
 
   /**
-   * When using this method you must call [[shutdownAll]] manually after you finished working with dependencies.
+   * When using this method you must call [[shutdownAll]] manually after you finished working with
+   * dependencies.
    */
-  def unsafeCreate(
-    runtime: IORuntime,
-    listener: AllocationLifecycleListener = NoOpListener,
-  ): Allocator =
-    new Allocator(runtime, listener)
+  def unsafeCreate(runtime: IORuntime): Allocator[IO] =
+    create[IO]().allocated.unsafeRunSync()(runtime)._1
 
 }
 
-class Allocator private(
-  runtime: IORuntime,
-  listener: AllocationLifecycleListener,
+class Allocator[F[_]: Sync] private (
+  dispatcher: Dispatcher[F],
+  shutdown: Ref[F, F[Unit]],
+  listener: AllocationLifecycleListener[F],
 ) {
 
-  private val shutdown: Ref[IO, IO[Unit]] = Ref.unsafe(IO.unit)
+  def withListener(listener: AllocationLifecycleListener[F]): Allocator[F] =
+    new Allocator(dispatcher, shutdown, listener)
 
-  def allocate[A: ClassTag](resource: Resource[IO, A]): A =
-    resource
-      .allocated
-      .flatMap { case (a, release) =>
-        listener.onInit(a) *>
-          shutdown.update(listener.onShutdown(a) *> release *> _) *> // Shutdown this resource, and after shutdown all previous
-          IO.pure(a)
-      }.unsafeRunSync()(runtime)
+  def allocate[A: ClassTag](resource: Resource[F, A]): A =
+    dispatcher.unsafeRunSync {
+      resource
+        .allocated
+        .flatMap { (acquired, release) =>
+          listener.onInit(acquired) *>
+            shutdown.update(listener.onShutdown(acquired) *> release *> _) *>
+            // Shutdown this resource, and after shutdown all previous
+            Sync[F].pure(acquired)
+        }
+    }
 
-  def allocate[A: ClassTag](io: IO[A]): A = allocate(io.toResource)
+  def allocate[A: ClassTag](fa: F[A]): A = allocate(fa.toResource)
 
-  def shutdownAll: IO[Unit] = {
-    shutdown.getAndSet(IO.unit).flatten
-  }
+  def shutdownAll: F[Unit] =
+    shutdown.getAndSet(Sync[F].unit).flatten
 
 }
